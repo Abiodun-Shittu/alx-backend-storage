3f5f909813a449b68761fd2387b3ef053ebc97a0
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-05 01:48:39 +0100

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that lists all bands with Glam rock
 SELECT band_name,
-	(IFNULL(split, '2020') - formed) AS lifespan
+	(YEAR(split) - YEAR(formed)) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
