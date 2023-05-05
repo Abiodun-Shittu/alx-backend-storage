f40fa18b02bd523c225bbecd96dc528e78b0948e
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-05 01:52:47 +0100

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,8 +1,11 @@
 -- Write a SQL script that creates a trigger
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
 CREATE TRIGGER decrease_quantity
 AFTER
 INSERT ON orders FOR EACH ROW BEGIN
 UPDATE items
 SET quantity = quantity - NEW.quantity
 WHERE id = NEW.item_id;
-END;
+END $$
+DELIMITER ;
