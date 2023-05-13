91d3af143f4b94cc52bd87efd541512f4a259147
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-13 06:42:51 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,17 +2,29 @@
 """
 Task: Working with Redis
 """
-import uuid
 import redis
 from typing import Union
 
 
 class Cache:
-    def __init__(self) -> None:
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def __init__(self, host='localhost', port=6379, db=0):
+        self._redis = redis.Redis(host=host, port=port, db=db)
+    
+    def store(self, value: Union[str, bytes, int]) -> str:
+        key = self._redis.incr('cache:key-counter')
+        self._redis.set(f'cache:key-{key}', value)
+        return str(key)
+    
+    def get(self, key: str, fn: Optional[Callable[[bytes], any]] = None) -> Union[str, bytes, int, None]:
+        value = self._redis.get(f'cache:key-{key}')
+        if value is None:
+            return None
+        if fn is not None:
+            value = fn(value)
+        return value
+    
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+    
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=lambda x: int(x))
